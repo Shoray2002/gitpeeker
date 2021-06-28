b8495f9e422e0238a2ddbca0b98164911120f312
Shoray Singhal <lci2020037@iiitl.ac.in>
2021-06-28 14:42:01 +0530

    initial commt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+  // console.log(search)
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (search === "") return;
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+    // console.log(json);
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo);
+    console.log(results);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <header>
+          <h1>wiki peeker</h1>
+          <form className="search-box" onSubmit={handleSearch}>
+            <input
+              type="search"
+              placeholder="Type to look for Wikis..."
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </form>
+          {searchInfo.totalhits ? (
+            <p>Search results:{searchInfo.totalhits}</p>
+          ) : (
+            ""
+          )}
+        </header>
+        <div className="results">
+          {results.map((result, i) => {
+            const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+            return (
+              <div className="result" key={i}>
+                <h3>{result.title}</h3>
+                <p dangerouslySetInnerHTML={{ __html: result.snippet }} />
+
+                <a href={url} target="_black" rel="noreferrer">Read More</a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
   );
 }
 
